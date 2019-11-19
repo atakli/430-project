7a40e7e9a311eb5f43b96c063fc63380c781615c
Emre ATAKLI <e211294@metu.edu.tr>
2019-11-19 17:17:23 +0300

    updated and got better understanding

--- Untitled.m
+++ Untitled.m
@@ -1,24 +1,25 @@
 % ********************* Define transmitted signal *************************
 N=100; % Number of bits , size of transmitted signal x_inp=[x_1 x_2 ... x_N] 
-x_inp = randi([0,1],1,N)%(1,N);  % binary signal 0 or 1 % message to be transmitted
+x_inp = randi([0,1],1,N);%(1,N);  % binary signal 0 or 1 % message to be transmitted
 preamble_N = 20;
 % preamble = randi([0,1],1,preamble_N)
 preamble = [0,1,0,0,1,1,1,0,1,1,0,1,0,1,0,1,1,0,1,0];
-N = preamble_N + N;
+N = preamble_N + N;     % toplam number of bits becomes 120
 x_inp = [x_inp preamble];
 % x_inp = [x_inp  preamble];
-Tb=0.0001; % bit period (second)   
+Tb=0.0001; % bit period (second)  
 % ********************* Represent input signal as digital signal ****
 x_bit=[]; 
 % L = 100;
-nb=100; % bbit/bit
+nb=100; % bbit/bit      % heralde samples/bit ya. Total number of samples becomes N * nb = 12000
+                        % total duration of signal becomes 12000 * Tb = 1.2 sec as the real case.
 for n=1:1:N   % 
-    if x_inp(n)==1;  % 
+    if x_inp(n)==1 
        x_bitt=ones(1,nb);
-    else x_inp(n)==0;
+    else x_inp(n)
         x_bitt=zeros(1,nb);
     end
-     x_bit=[x_bit x_bitt];		% her loop'tan sonra üstüne ekliyo
+     x_bit=[x_bit x_bitt];		% her loop'tan sonra ustune ekliyo
 end
 % x_bit = upsample(x_inp,L);
 t1=Tb/nb:Tb/nb:nb*N*(Tb/nb); % time of the signal 
@@ -83,10 +84,10 @@ end
 x_out=y_dem; % output signal;
 % *************** Represent output signal as digital signal ***************
 xx_bit=[];
-for n=1:length(x_out);
-    if x_out(n)==1;
+for n=1:length(x_out)
+    if x_out(n)==1
        xx_bitt=ones(1,nb);
-    else x_out(n)==0;
+    else x_out(n)
         xx_bitt=zeros(1,nb);
     end
      xx_bit=[xx_bit xx_bitt];
--- demod.m
+++ demod.m
@@ -6,7 +6,7 @@ plot(time,y);
 % ylim([-0.2 1.2]);
 
 figure;
-Tb = 1/Fs;
+Tb_re = 1/Fs * nb ;
 % ********************* Channel model h and w *****************************
 % h=1; % Fading 
 % w=0; % Noise
@@ -15,12 +15,12 @@ Tb = 1/Fs;
 % ********************* Define BPSK Demodulation **************************
 y_dem=[];
 for n=t2L:t2L:length(y)
-  t=Tb/nb:Tb/nb:Tb;
-  c=cos(2*pi*fc*t); % carrier siignal 
+  t=Tb_re/nb:Tb_re/nb:Tb_re;
+  c=cos(2*pi*fc*t); % carrier signal 
   y_dem0=c.*y((n-(t2L-1)):n);
-  t4=Tb/nb:Tb/nb:Tb;
-  z=trapz(t4,y_dem0); % intregation 
-  A_dem=round((2*z/Tb));                                     
+  t4=Tb_re/nb:Tb_re/nb:Tb_re;
+  z=trapz(t4,y_dem0); % integration 
+  A_dem=round((2*z/Tb_re));                                     
   if(A_dem>Ac/2) % logic level = Ac/2
     A=1;
   else
@@ -31,18 +31,18 @@ end
 x_out=y_dem; % output signal;
 % *************** Represent output signal as digital signal ***************
 xx_bit=[];
-for n=1:length(x_out);
-    if x_out(n)==1;
+for n=1:length(x_out)
+    if x_out(n)==1
        xx_bitt=ones(1,nb);
-    else x_out(n)==0;
+    else x_out(n)
         xx_bitt=zeros(1,nb);
     end
      xx_bit=[xx_bit xx_bitt];
 end
-t4=Tb/nb:Tb/nb:nb*length(x_out)*(Tb/nb);
-subplot(3,1,3)
+t4=Tb_re/nb:Tb_re/nb:nb*length(x_out)*(Tb_re/nb);
+% subplot(3,1,3)
 plot(t4,xx_bit,'LineWidth',2);grid on;
-axis([ 0 Tb*length(x_out) -0.5 1.5]);
+axis([ 0 Tb_re*length(x_out) -0.5 1.5]);
 ylabel('Amplitude(volt)');
 xlabel(' Time(sec)');
 title('Output signal as digital signal');
