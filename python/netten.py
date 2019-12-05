1d7998a88a24ec664fcbeb0584ffcdb8e463baf3
Emre ATAKLI <e211294@metu.edu.tr>
2019-12-05 22:52:12 +0300

    update

--- python/netten.py
+++ python/netten.py
@@ -9,9 +9,11 @@ from math import pi,floor
 import matplotlib.pyplot as plt
 import matplotlib
 from scipy.io.wavfile import write,read
+
 zhfont1 = matplotlib.font_manager.FontProperties(fname = 'C:\Windows\Fonts\simsun.ttc')
-fig, (ax1,ax2,ax3) = fig.subplots(3, 1, 3)
+fig, (ax1,ax2,ax3) = plt.subplots(3, 1)
 fig.subplots_adjust(hspace=1)
+
 def dosyadan_oku():
     Fs,y = read(r'C:\Users\Emre\Desktop\14.08.09_the_mechanic\ders\430\proje\430-project\python\den.wav')
     
@@ -34,8 +36,8 @@ def random():
     
      # Randomly generated signal sequence
     a = np.random.randint(0, 2, size)
-    m = np.zeros(size * nb, dtype=np.float32)
-    for i in range(size * nb):
+    m = np.zeros(int(size * nb), dtype=np.float32)
+    for i in range(int(size * nb)):
         m[i] = a[floor(t[i])]
         
     fc = 4000
@@ -45,10 +47,15 @@ def random():
     bpsk = np.cos(np.dot(2 * pi * fc, ts) + pi * (m - 1))
      # AWGN noise
     noise_bpsk = awgn(bpsk, 5)
+    yaz(quantization_level,noise_bpsk,Fs)
+    plot_input(m)
+    plot_bpsk(bpsk)
+    plot_noised(noise_bpsk)
+    
 def plot_input(input_sound):
     ax1.set_title('Generate random n-bit binary signal', fontproperties = zhfont1, fontsize = 20)
 #    plt.axis([0, size, -0.5, 1.5])
-    plt.plot(input_sound, 'b')
+    ax1.plot(input_sound, 'b')
     
 def plot_bpsk(bpsk):
     ax2.set_title('BPSK modulation signal', fontproperties=zhfont1, fontsize=20) #  width=0.5,
@@ -59,6 +66,8 @@ def plot_noised(noise_bpsk):
     ax3.set_title('BPSK modulated signal superimposed noise waveform', fontproperties = zhfont1, fontsize = 20)
 #    ax3.axis([0, size, -1.5, 1.5])
     ax3.plot(noise_bpsk, 'r')
-def yaz(quantization_level,noise_bpsk):
+    
+def yaz(quantization_level,noise_bpsk,Fs):
     bunu_yaz = np.int16(noise_bpsk/np.max(np.abs(noise_bpsk)) * 2**(quantization_level-1))
     write('bpsk.wav',Fs,bunu_yaz)
+random()
