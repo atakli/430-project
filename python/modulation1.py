3ee7b884a07fd292dececb9a9400ac6c55b60a57
Emre ATAKLI <e211294@metu.edu.tr>
2019-12-11 15:13:19 +0300

    update

--- python/modulation1.py
+++ python/modulation1.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Dec    5 09:35:53 2019
+Created on Thu Dec	  5 09:35:53 2019
 @author: Emre
 """
 # -*- coding:utf-8 -*-
@@ -15,88 +15,90 @@ bas = time.time()
 zhfont1 = matplotlib.font_manager.FontProperties(fname = 'C:\Windows\Fonts\simsun.ttc')
 
 def main():
-    Fs,y_den = read(r'C:\Users\Emre\Desktop\14.08.09_the_mechanic\ders\430\proje\430-project\python\den.wav')
-    y_den = y_den[:,0]
-    #result = filter(y_den,low=50,high=150,typ='bandpass')
-    #result = np.int16(result)
-    write('den0.wav',Fs,y_den)
-    y_b = np.fromfile(r'C:\Users\Emre\Desktop\14.08.09_the_mechanic\ders\430\proje\430-project\python\den0.wav',
-                          dtype = "uint8")
-    y_bytes = np.unpackbits(y_b)
-    print('uzunluk: ',len(y_bytes))
-#    y_bytes = y_bytes[:100000] # 2982240
-#    size = len(y_bytes)
-    sampling_t = 0.1
-    nb = int(1/sampling_t)
-    t = np.arange(0, len(y_bytes), sampling_t)
-    t_floor = np.int64(np.floor(t))
-    m = y_bytes[t_floor]
-    print("m'nin uzunluğu: ",len(m))
-    # file_stats = os.stat(r'C:\Users\Emre\Desktop\14.08.09_the_mechanic\ders\430\proje\430-project\python\den0.wav')
-    # quantization_level = int(file_stats.st_size * 8 / y.shape[0]) # how many bits used to represent every sample
-    quantization_level = 16
-    fc = 4000
-    ts = np.arange(0, 1 / sampling_t * len(y_bytes) / Fs, 1 / Fs)
-    bpsk = np.cos(np.dot(2 * pi * fc, ts) + pi * (m - 1))
-    bunu_yaz = np.int16(bpsk/np.max(np.abs(bpsk)) * 2**(quantization_level-1))
-    write('bpsk_de.wav',Fs,bunu_yaz)
-    
+	Fs,y_den = read(r'C:\Users\Emre\Desktop\14.08.09_the_mechanic\ders\430\proje\430-project\python\den.wav')
+	y_den = y_den[:,0]
+	#result = filter(y_den,low=50,high=150,typ='bandpass')
+	#result = np.int16(result)
+	write('den0.wav',Fs,y_den)
+	y_b = np.fromfile(r'C:\Users\Emre\Desktop\14.08.09_the_mechanic\ders\430\proje\430-project\python\den0.wav',
+						  dtype = "uint8")
+	y_bytes = np.unpackbits(y_b)
+	print('uzunluk: ',len(y_bytes))
+#	 y_bytes = y_bytes[:100000] # 2982240
+#	 size = len(y_bytes)
+	sampling_t = 0.1
+	nb = int(1/sampling_t)
+	t = np.arange(0, len(y_bytes), sampling_t)
+	t_floor = np.int64(np.floor(t))
+	m = y_bytes[t_floor]
+	print("m'nin uzunluğu: ",len(m))
+	# file_stats = os.stat(r'C:\Users\Emre\Desktop\14.08.09_the_mechanic\ders\430\proje\430-project\python\den0.wav')
+	# quantization_level = int(file_stats.st_size * 8 / y.shape[0]) # how many bits used to represent every sample
+	quantization_level = 16
+	fc = 4000
+	ts = np.arange(0, 1 / sampling_t * len(y_bytes) / Fs, 1 / Fs)
+	bpsk = np.cos(np.dot(2 * pi * fc, ts) + pi * (m - 1))
+	# plt.plot(bpsk)
+	# plt.show()
+	bunu_yaz = np.int16(bpsk/np.max(np.abs(bpsk)) * 2**(quantization_level-1))
+	write('bpsk_de.wav',Fs,bunu_yaz)
+	# __import__('pdb').set_trace()
 def random():
-    
-    size = 10 # 10
-    #sampling_t = 1/Fs
-    sampling_t = 0.1
-    nb = 1 / sampling_t
-    quantization_level = 16 # 16 : 64 kbytes
-    t = np.arange(0, size, sampling_t)
-    
-    # Define additive white Gaussian noise
-    def awgn(y, snr):
-    
-        snr = 10 ** (snr / 10.0)
-        xpower = np.sum(y ** 2) / len(y)
-        npower = xpower / snr
-        return np.random.randn(len(y)) * np.sqrt(npower) + y
-    
-     # Randomly generated signal sequence
-    a = np.random.randint(0, 2, size)
-    m = np.zeros(int(size * nb), dtype=np.float32)
-    for i in range(int(size * nb)):
-        m[i] = a[floor(t[i])]
-        
-    fc = 4000
-    Fs = 20 * fc # sampling frequency
-    ts = np.arange(0, (nb * size) / Fs, 1 / Fs)
-     # BPSK modulated signal waveform
-    bpsk = np.cos(np.dot(2 * pi * fc, ts) + pi * (m - 1))
-     # AWGN noise
-    noise_bpsk = awgn(bpsk, 5)
-    yaz_ve_cal(quantization_level,noise_bpsk,Fs)
-#    fig, ax = plt.subplots(3, 1)
-#    fig.subplots_adjust(hspace=1)
-#    plot_input(ax,m)
-#    plot_bpsk(ax,bpsk)
-#    plot_noised(ax,noise_bpsk)
-    return m
+	
+	size = 10 # 10
+	#sampling_t = 1/Fs
+	sampling_t = 0.1
+	nb = 1 / sampling_t
+	quantization_level = 16 # 16 : 64 kbytes
+	t = np.arange(0, size, sampling_t)
+	
+	# Define additive white Gaussian noise
+	def awgn(y, snr):
+	
+		snr = 10 ** (snr / 10.0)
+		xpower = np.sum(y ** 2) / len(y)
+		npower = xpower / snr
+		return np.random.randn(len(y)) * np.sqrt(npower) + y
+	
+	 # Randomly generated signal sequence
+	a = np.random.randint(0, 2, size)
+	m = np.zeros(int(size * nb), dtype=np.float32)
+	for i in range(int(size * nb)):
+		m[i] = a[floor(t[i])]
+		
+	fc = 4000
+	Fs = 20 * fc # sampling frequency
+	ts = np.arange(0, (nb * size) / Fs, 1 / Fs)
+	 # BPSK modulated signal waveform
+	bpsk = np.cos(np.dot(2 * pi * fc, ts) + pi * (m - 1))
+	 # AWGN noise
+	noise_bpsk = awgn(bpsk, 5)
+	yaz_ve_cal(quantization_level,noise_bpsk,Fs)
+#	 fig, ax = plt.subplots(3, 1)
+#	 fig.subplots_adjust(hspace=1)
+#	 plot_input(ax,m)
+#	 plot_bpsk(ax,bpsk)
+#	 plot_noised(ax,noise_bpsk)
+	return m
 def plot_input(ax,input_sound):
-    ax[0].set_title('Generated random n-bit binary signal', fontproperties = zhfont1, fontsize = 20)
-#     plt.axis([0, size, -0.5, 1.5])
-    ax[0].plot(input_sound, 'b')
-    
+	ax[0].set_title('Generated random n-bit binary signal', fontproperties = zhfont1, fontsize = 20)
+#	  plt.axis([0, size, -0.5, 1.5])
+	ax[0].plot(input_sound, 'b')
+	
 def plot_bpsk(ax,bpsk):
-    ax[1].set_title('BPSK modulated signal', fontproperties=zhfont1, fontsize=20) #     width=0.5,
-#     ax2.axis([0,size,-1.5, 1.5])
-    ax[1].plot(bpsk, 'r')
-    
+	ax[1].set_title('BPSK modulated signal', fontproperties=zhfont1, fontsize=20) #		width=0.5,
+#	  ax2.axis([0,size,-1.5, 1.5])
+	ax[1].plot(bpsk, 'r')
+	
 def plot_noised(ax,noise_bpsk):
-    ax[2].set_title('BPSK modulated signal superimposed noise waveform', fontproperties = zhfont1, fontsize = 20)
-#     ax3.axis([0, size, -1.5, 1.5])
-    ax[2].plot(noise_bpsk, 'r')
-    
+	ax[2].set_title('BPSK modulated signal superimposed noise waveform', fontproperties = zhfont1, fontsize = 20)
+#	  ax3.axis([0, size, -1.5, 1.5])
+	ax[2].plot(noise_bpsk, 'r')
+	
 def yaz_ve_cal(quantization_level,noise_bpsk,Fs):
-    bunu_yaz = np.int16(noise_bpsk/np.max(np.abs(noise_bpsk)) * 2**(quantization_level-1))
-    write('bpsk.wav',Fs,bunu_yaz)
-#    playsound('bpsk.wav')
+	bunu_yaz = np.int16(noise_bpsk/np.max(np.abs(noise_bpsk)) * 2**(quantization_level-1))
+	write('bpsk.wav',Fs,bunu_yaz)
+#	 playsound('bpsk.wav')
 #neymis = random()
 main()
 print('toplam süre: ',time.time()-bas)
--- python/netten.py
+++ python/netten.py
@@ -12,7 +12,8 @@ from math import pi
 import matplotlib.pyplot as plt
 import matplotlib,time,math
 import scipy.signal as signal
-
+import copy
+# import pdb
 size = 1000
 sampling_t = 0.01
 nb = int(1 / sampling_t)
@@ -20,10 +21,14 @@ t = np.arange(0, size, sampling_t)
 
  # Randomly generated signal sequence
 a = np.random.randint(0, 2, size)
-m = np.zeros(len(t), dtype=np.float32)
+# m = np.zeros(len(t), dtype=np.float32)
 
-for i in range(len(t)):
-    m[i] = a[math.floor(t[i])]
+# for i in range(len(t)):
+    # m[i] = a[math.floor(t[i])]
+	
+t_floor = np.int64(np.floor(t))
+m = a[t_floor]
+
 fig = plt.figure()
 ax1 = fig.add_subplot(3, 1, 1)
 
@@ -77,57 +82,28 @@ coherent_demod = bandpass_out * (coherent_carrier * 2)
 
  # Pass low pass filter
 lowpass_out = signal.filtfilt(b12, a12, coherent_demod)
+lowpass_out0 = copy.deepcopy(lowpass_out)
 fig2 = plt.figure()
 bx1 = fig2.add_subplot(3, 1, 1)
 bx1.set_title('local carrier downconversion, after low pass filter', fontproperties = zhfont1, fontsize=20)
 plt.axis([0, size, -1.5, 1.5])
 plt.plot(t, lowpass_out, 'r')
 
- #sample judgment
 detection_bpsk = np.zeros(len(t), dtype=np.float32)
-flag = np.zeros(size, dtype=np.float32)
-
-bak1 = time.time()
-for i in range(size):
-    tempF = 0
-    for j in range(100):
-        tempF = tempF + lowpass_out[i * 100 + j]
-    if tempF > 0:
-        flag[i] = 1
-    else:
-        flag[i] = 0
-for i in range(size):
-    if flag[i] == 0:
-        for j in range(100):
-            detection_bpsk[i * 100 + j] = 0
-    else:
-        for j in range(100):
-            detection_bpsk[i * 100 + j] = 1
-print('eskisi: ',time.time()-bak1)
-detection_bpsk3 = np.zeros(size, dtype=np.float32)
-tempF = np.zeros(0, dtype=np.float32)
-
-bak = time.time()
-for i in range(size):
-    tempF = np.append(tempF,lowpass_out[i*nb:(i+1)*nb].sum())
-detection_bpsk3 = np.where(tempF > 0,1,0)
-print('yenisi: ',time.time()-bak)
-print('aynı mı: ',np.array_equal(detection_bpsk3,a))
-detection_bpsk2 = np.where(lowpass_out > 0,1,0)
+###
+lowpass_out = lowpass_out.reshape((int(len(lowpass_out)/nb),nb))
+tempF = np.sum(lowpass_out,axis=1)
+detection_bpsk = detection_bpsk.reshape((int(len(detection_bpsk)/nb),nb))
+detection_bpsk[:] = np.where(tempF > 0,1,0).reshape((len(tempF),1))
+detection_bpsk = detection_bpsk.flatten()
+###
+detection_bpsk2 = np.where(lowpass_out0 > 0,1,0)
+###
 print('ne kadarı aynı: Yüzde ',len(np.where(detection_bpsk == detection_bpsk2)[0])/len(detection_bpsk)*100)
 bx2 = fig2.add_subplot(3, 1, 2)
 bx2.set_title('signal after BPSK signal sampling decision', fontproperties = zhfont1, fontsize=20)
 plt.axis([0, size, -0.5, 1.5])
 plt.plot(t, detection_bpsk, 'r')
 plt.tight_layout()
-
-fig3 = plt.figure()
-cx1 = fig3.add_subplot(3, 1, 1)
-cx1.set_title('new signal', fontproperties = zhfont1, fontsize=20)
-plt.plot(detection_bpsk3, 'g')
-
-cx2 = fig3.add_subplot(3, 1, 2)
-cx2.set_title('original signal', fontproperties = zhfont1, fontsize=20)
-plt.plot(a, 'g')
-plt.tight_layout()
-plt.show()
+# plt.show()
+# pdb.set_trace()
